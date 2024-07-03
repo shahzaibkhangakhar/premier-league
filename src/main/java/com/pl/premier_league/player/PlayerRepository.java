9da6e80eab629dd0acea591fcd3006495542ad8e
Shahzaib Gakhar <shahzaibgakhar@Shahzaibs-MacBook-Pro.local>
2024-07-04 00:13:25 +0200

    first commit

---
+++ src/main/java/com/pl/premier_league/player/PlayerRepository.java
@@ -0,0 +1,16 @@
+package com.pl.premier_league.player;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, String> {
+    void deleteByName(String playerName);
+
+    Optional<Player> findByName(String name);
+}
